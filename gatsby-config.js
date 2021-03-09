cadca9f04f832acc723f4418fd11c1f55481e63f
pkolt <pkoltyshev@gmail.com>
2021-03-09 22:50:18 +0500

    Add yandex-metrika

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,23 @@ module.exports = {
         showDate: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-yandex-metrika`,
+      options: {
+        // The ID of yandex metrika.
+        trackingId: 42089099,
+        // Enabled a webvisor. The default value is `false`.
+        webvisor: false,
+        // Enables tracking a hash in URL. The default value is `false`.
+        trackHash: false,
+        // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
+        // and `true` means after the body (faster loading, less hits). The default value is `false`.
+        afterBody: true,
+        // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
+        // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+        defer: false,
+      },
+    },
     'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
   ],
