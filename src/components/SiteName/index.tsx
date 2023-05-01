d560033eb65c30be887c7aaa6878ed6bcad17be8
pkolt <pkoltyshev@gmail.com>
2023-05-01 22:05:48 +0400

    Fix animation

--- src/components/SiteName/index.tsx
+++ src/components/SiteName/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.css';
 export const SiteName: React.FC = () => {
   return (
     <Link href="/" className={styles.container}>
-      <SiteLogo />
+      <SiteLogo width={50} height={50} />
       <h1 className={styles.name}>Developer Blog</h1>
       <p className={styles.description}>Pavel Koltyshev</p>
     </Link>
