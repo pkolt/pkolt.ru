d6b6d04397ca62010143e3b7b47a68464f59f91f
pkolt <pkoltyshev@gmail.com>
2023-05-28 13:28:49 +0400

    Fix title

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from 'next';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+    title: 'Страница не найдена (404)',
+  };
 
 const NotFound: React.FC = () => {
   return (
