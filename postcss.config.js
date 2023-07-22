55998b64b615bb0277ef34cd1245a6fe5f45ee0c
pkolt <pkoltyshev@gmail.com>
2023-07-22 13:19:32 +0400

    Fix colors

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,29 @@
 /** @type {import('@csstools/postcss-global-data').pluginOptions} */
 const globalDataOptions = {
-  files: [
-    './src/styles/media-queries.css',
-  ],
+  files: ['./src/styles/media-queries.css'],
+};
+
+/** @type {import('@csstools/postcss-oklab-function').pluginOptions} */
+const oklabFuncOptions = {
+  preserve: true,
 };
 
 /** @type {import('postcss-preset-env').pluginOptions} */
 const presentEnvOptions = {
   // debug: true,
+  // browsers: '> 0.2% and not dead',
+  // minimumVendorImplementations: 2,
+  stage: false, // Disable all features
+  features: {
+    'nesting-rules': true,
+    'custom-media-queries': true,
+  }
 };
 
 const config = {
   plugins: [
     ['@csstools/postcss-global-data', globalDataOptions],
+    ['@csstools/postcss-oklab-function', oklabFuncOptions],
     ['postcss-preset-env', presentEnvOptions],
   ],
 };
