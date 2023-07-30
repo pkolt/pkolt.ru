61ca058157d16d4e98e886aee57261ac71a222c2
Pavel Koltyshev <pkoltyshev@gmail.com>
2023-07-30 11:35:34 +0400

    18-dark-theme (#75)
    * Update deps
    
    * Fix dark theme

--- src/components/SiteLogo/index.tsx
+++ src/components/SiteLogo/index.tsx
@@ -1,15 +1,17 @@
-import LogoImage from '../../../public/images/icons/logo.svg';
+/* eslint-disable @next/next/no-img-element */
+import { CSSClassProps } from '@/types/components';
 import styles from './index.module.css';
+import cn from 'classnames';
 
-interface SiteLogoProps {
-  width: number;
-  height: number;
-}
-
-export const SiteLogo: React.FC<SiteLogoProps> = ({ width, height }) => {
+export const SiteLogo: React.FC<CSSClassProps> = ({ className }) => {
   return (
-    <div className={styles.container} style={{ width: `${width}px`, height: `${height}px` }}>
-      <LogoImage width={width} height={height} alt="Site logo" />
+    <div className={cn(styles.container, className)}>
+      <div className={styles.image}>
+        <picture>
+          <source srcSet="/images/images/logo@1x.png 1x, /images/images/logo@2x.png 2x" type="image/png" />
+          <img src="/images/images/logo@1x.png" width="32px" height="50px" alt="Site logo" />
+        </picture>
+      </div>
     </div>
   );
 };
