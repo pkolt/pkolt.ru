44bfac443c5ac9df034e84a834c9c1b9e20cab6b
pkolt <pkoltyshev@gmail.com>
2023-08-16 18:23:05 +0400

    Fix headers

--- src/components/Headers/index.tsx
+++ src/components/Headers/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.css';
 
 type HeaderType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-const createHeaderBlock = (elemType: HeaderType) => {
+const createHeader = (elemType: HeaderType) => {
   const HeaderBlock = ({ children }: React.PropsWithChildren): React.ReactElement => {
     const id = slugify(`${children}`, { lower: true });
     const className = styles.container;
@@ -21,12 +21,9 @@ const createHeaderBlock = (elemType: HeaderType) => {
   };
   return HeaderBlock;
 };
-
-export const HeadersBlock = {
-  H1: createHeaderBlock('h1'),
-  H2: createHeaderBlock('h2'),
-  H3: createHeaderBlock('h3'),
-  H4: createHeaderBlock('h4'),
-  H5: createHeaderBlock('h5'),
-  H6: createHeaderBlock('h6'),
-};
+export const H1 = createHeader('h1');
+export const H2 = createHeader('h2');
+export const H3 = createHeader('h3');
+export const H4 = createHeader('h4');
+export const H5 = createHeader('h5');
+export const H6 = createHeader('h6');
