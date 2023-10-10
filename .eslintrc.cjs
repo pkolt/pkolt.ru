0a87332b250a90445722862673ec3ddbcb0bdd45
pkolt <pkoltyshev@gmail.com>
2023-10-10 21:38:53 +0400

    Fix format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -39,6 +39,7 @@ module.exports = {
         // "astro/no-set-html-directive": "error"
         'react/no-unknown-property': 0,
         'react/jsx-filename-extension': 0,
+        'perfectionist/sort-astro-attributes': 0,
       },
     },
   ],
