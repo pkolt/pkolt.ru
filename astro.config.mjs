300600c0c0caeec56043062959e865d1b492e870
pkolt <pkoltyshev@gmail.com>
2023-10-19 21:29:51 +0400

    Fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,46 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { DateTime } from 'luxon';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import remarkYoutube from 'remark-youtube';
 
+const SITE_URL = 'https://pkolt.ru';
+
 /** @type {import('astro/config').AstroUserConfig} */
 const config = {
-  site: 'https://pkolt.ru',
-  integrations: [react(), sitemap()],
+  site: SITE_URL,
+  integrations: [
+    react(),
+    sitemap({
+      priority: 0.6, // default <priority>
+      changefreq: 'daily', // default <changefreq>
+      filter: (url) => !url.endsWith('/about/'),
+      serialize: (item) => {
+        if (item.url.endsWith('/')) {
+          item.url = item.url.slice(0, -1);
+        }
+
+        if (item.url === SITE_URL) {
+          item.priority = 1.0;
+          item.lastmod = DateTime.now().toISO();
+        }
+
+        if (item.url.includes('/blog/')) {
+          item.priority = 0.8;
+          const postFile = path.resolve('./', 'src/pages', `${new URL(item.url).pathname.slice(1)}.md`);
+          const postContent = fs.readFileSync(postFile, 'utf8');
+          const modified = /modified:\s+(\d{4}-\d{2}-\d{2})/.exec(postContent)[1];
+          if (modified) {
+            item.lastmod = DateTime.fromSQL(modified).toISO();
+          }
+        }
+
+        return item;
+      },
+    }),
+  ],
   compressHTML: true,
   markdown: {
     remarkPlugins: [remarkYoutube],
