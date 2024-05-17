b482653fb4b6f7e2cbdfd3fb1a2c84aeb54df254
pkolt <pkoltyshev@gmail.com>
2024-05-17 21:42:29 +0400

    Remove PWA

--- knip.js
+++ knip.js
@@ -1,7 +1,7 @@
 /** @type {import('knip').KnipConfig} */
 const config = {
   entry: ['src/**/*.{ts,tsx,astro,md,mdx}', 'astro.config.mjs', 'postcss.config.cjs'],
-  ignore: [],
+  ignore: ['./public/service-worker.js', './public/service-worker-register.js'],
   ignoreDependencies: ['@typescript-eslint/eslint-plugin', 'sharp'],
 };
 
