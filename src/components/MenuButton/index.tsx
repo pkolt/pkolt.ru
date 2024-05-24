e5ba14b6d8fcc431acfa773bf01544cac5c3585c
Pavel Koltyshev <pkoltyshev@gmail.com>
2024-05-24 22:10:09 +0400

    150-remove-mui (#151)
    * Remove mui
    
    * Fix mobile panel
    
    * Update deps
    
    * Remove unused deps

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -1,43 +1,48 @@
-import { Modal, ModalClose, ModalDialog } from '@mui/joy';
 import cn from 'classnames';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
 
-import { MUIWrapper } from '../MUIWrapper';
 import { Navigation } from '../Navigation';
 import { SocialNav } from '../SocialNav';
 import styles from './index.module.css';
 
-const DELAY_MS = 250;
-
 export const MenuButton = (): JSX.Element => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   const [opened, setOpened] = useState(false);
-  const [openDialog, setOpenDialog] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   const handleOpen = useCallback(() => {
-    setOpened(true);
-    setTimeout(() => setOpenDialog(true), DELAY_MS);
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+      setOpened(true);
+      document.body.style.overflow = 'hidden';
+    }
   }, []);
 
   const handleClose = useCallback(() => {
-    setOpenDialog(false);
-    setTimeout(() => setOpened(false), DELAY_MS);
+    if (dialogRef.current) {
+      dialogRef.current.close();
+      setOpened(false);
+      document.body.style.overflow = '';
+    }
   }, []);
 
-  //! Replace Modal to Drawer (https://mui.com/joy-ui/react-drawer/)
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <>
       <i className={cn('bi-list', styles.icon, opened && styles.opened)} onClick={handleOpen} role="button" />
-      <MUIWrapper>
-        <Modal onClose={handleClose} open={openDialog}>
-          <ModalDialog sx={{ height: '100%', width: '90%' }}>
-            <div className={styles.modal}>
-              <ModalClose />
-              <Navigation />
-              <SocialNav />
-            </div>
-          </ModalDialog>
-        </Modal>
-      </MUIWrapper>
+      {isMounted &&
+        createPortal(
+          <dialog className={styles.dialog} ref={dialogRef}>
+            <i className={cn('bi-x', styles.close)} role="button" onClick={handleClose} tabIndex={-1} />
+            <Navigation />
+            <SocialNav />
+          </dialog>,
+          document.body,
+        )}
     </>
   );
 };
