2e7c48fb91d6522154193de3ac19411637762421
pkolt <pkoltyshev@gmail.com>
2025-01-31 12:09:56 +0500

    Fixed react router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,8 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
   appDirectory: 'src/app',
+  prerender() {
+    return ['/'];
+  },
 } satisfies Config;
