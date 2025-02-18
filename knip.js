1b336f861d53a78967476ef41bec53f4fb56dacd
pkolt <pkoltyshev@gmail.com>
2025-02-18 11:27:53 +0500

    Fixed knip

--- knip.js
+++ knip.js
@@ -1,8 +1,8 @@
 /** @type {import('knip').KnipConfig} */
 const config = {
-  entry: ['src/**/*.{ts,tsx,astro,md,mdx}', 'astro.config.mjs', 'postcss.config.cjs'],
+  entry: ['src/**/*.{ts,tsx}', 'react-router.config.ts', 'postcss.config.js'],
   ignore: ['./public/service-worker.js', './public/service-worker-register.js'],
-  ignoreDependencies: ['@typescript-eslint/eslint-plugin', 'sharp'],
+  ignoreDependencies: ['@react-router/node', 'isbot', 'eslint-import-resolver-typescript', 'react-router-devtools'],
 };
 
 export default config;
